1656f56926fe64a52e2d14b12ea0f693bd2dd415
Kazem <kazemgh.hookman@gmail.com>
2022-04-29 14:45:35 +0430

    add multicall

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from webbrowser import get
 import web3
 from abi import twap_abi, dei_pool_abi
 from pprint import pprint
+from multicall.multicall import Multicall
+
+from multicall.call import Call
 
 w3 = web3.Web3(web3.HTTPProvider("https://rpc.ankr.com/fantom"))
-dei_pool = w3.eth.contract(w3.toChecksumAddress("0x9bd5cc542bc922e95ba41c0702555e830f2c1cb4"), abi=dei_pool_abi)
-twap_contract = w3.eth.contract(w3.toChecksumAddress("0x1Bc270B2bE5c361784044ccE3f55c896fB5Fdf5A"), abi=twap_abi)
+dei_pool = w3.eth.contract(w3.toChecksumAddress(
+    "0x9bd5cc542bc922e95ba41c0702555e830f2c1cb4"), abi=dei_pool_abi)
+twap_contract = w3.eth.contract(w3.toChecksumAddress(
+    "0x1Bc270B2bE5c361784044ccE3f55c896fB5Fdf5A"), abi=twap_abi)
+
 
 def get_tx_data():
     data = open("data.csv")
@@ -17,7 +23,7 @@ def get_tx_data():
     can_collect = []
 
     collected_collateral = []
-    
+
     for line in content:
         line_data = line.split(",")
         from_address = line_data[4].replace("\"", "")
@@ -32,7 +38,7 @@ def get_tx_data():
             diff = _now - _time
             if (diff > 28800):  # can collect
                 can_collect.append(line_data[4].replace("\"", ""))
-        elif method == "\"Collect Collateral\"": # collected usdc
+        elif method == "\"Collect Collateral\"":  # collected usdc
             collected_collateral.append(from_address)
 
     redeemed = list(set(redeemed))
@@ -44,21 +50,33 @@ def get_tx_data():
 
 def to_be_collected_deus(can_collect):
     to_be_collected = []
+    info_calls = []
     for collector in can_collect:
-        positions = dei_pool.functions.getUnRedeemedPositions(w3.toChecksumAddress(collector)).call()
+        info_calls.append(
+            Call(
+                w3,
+                dei_pool.address,
+                ['getUnRedeemedPositions(address)((uint256,uint256)[])', w3.toChecksumAddress(
+                    collector)],
+                [[w3.toChecksumAddress(collector), None]]
+            )
+        )
+    info = Multicall(w3, info_calls)()
+    for collector, positions in info.items():
         total_usd = 0
         total_deus = 0
-        for position in positions:
+        for position in positions[0]:
             amount, __time = position
             amount = int(amount)
             __time = int(__time)
 
-            if __time == 0: continue
+            if __time == 0:
+                continue
 
-            deus_twap = twap_contract.functions.twap(w3.toChecksumAddress("0xde5ed76e7c05ec5e4572cfc88d1acea165109e44"), 10**18, __time, 28800).call()
-            
+            # deus_twap = twap_contract.functions.twap(w3.toChecksumAddress("0xde5ed76e7c05ec5e4572cfc88d1acea165109e44"), 10**18, __time, 28800).call()
+
             total_usd += amount
-            total_deus += int((amount * 10 ** 18)/ int(deus_twap))
+            # total_deus += int((amount * 10 ** 18)/ int(deus_twap))
 
         to_be_collected.append({
             'address': collector,
@@ -68,10 +86,12 @@ def to_be_collected_deus(can_collect):
 
     return to_be_collected
 
+
 def to_be_collected_collateral(un_collected):
     usdc_uncollected = []
     for not_collected in un_collected:
-        amount = dei_pool.functions.redeemCollateralBalances(w3.toChecksumAddress(not_collected)).call()
+        amount = dei_pool.functions.redeemCollateralBalances(
+            w3.toChecksumAddress(not_collected)).call()
         amount = int(amount)
         usdc_uncollected.append({
             'address': not_collected,
@@ -79,6 +99,7 @@ def to_be_collected_collateral(un_collected):
         })
 
     return usdc_uncollected
+
 
 redeemed, can_collect, collected_collateral, un_collected = get_tx_data()
 
@@ -91,4 +112,4 @@ summery = {
     "toBeCollectedUSDC": to_be_collected_collateral(un_collected)
 }
 
-print(summery)
+print(summery['toBeCollectedDeus'])
